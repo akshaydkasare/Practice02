08e688a826e0b22d0d524c10ed969c6a79cfcc4a
akshaydkasare <akshaydkasare@gmail.com>
2025-06-06 19:52:54 +0530

    TL commit code

--- GitProject01/src/main/java/com/nt/main/App.java
+++ GitProject01/src/main/java/com/nt/main/App.java
@@ -8,10 +8,6 @@ public class App {
         System.out.println("Hello World!");
         double b=20;
         System.out.println("============================");
-<<<<<<< HEAD
-        
-=======
->>>>>>> branch 'master' of https://github.com/akshaydkasare/Practice02.git
         System.out.println("Welcome to GitHub");
     }
 }
