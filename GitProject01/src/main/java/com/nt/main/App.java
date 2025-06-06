0e1e70c06a2ae457842355ce14b36aeaed2dd205
akshaydkasare <akshaydkasare@gmail.com>
2025-06-06 19:29:50 +0530

    TL add code and commit

--- GitProject01/src/main/java/com/nt/main/App.java
+++ GitProject01/src/main/java/com/nt/main/App.java
@@ -6,6 +6,7 @@ package com.nt.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        double b=20;
         System.out.println("Welcome to GitHub");
     }
 }
